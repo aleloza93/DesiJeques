4ea15b567d7032a9124489200ebc93ac3c44447e
unknown <colox.09@gmail.com>
2026-06-21 00:35:31 -0300

    falto el repo de HistorialEstadoFactura

---
+++ src/main/java/com/desi/jeques/repository/HistorialEstadoFacturaRepository.java
@@ -0,0 +1,9 @@
+package com.desi.jeques.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desi.jeques.entity.HistorialEstadoFactura;
+
+public interface HistorialEstadoFacturaRepository extends JpaRepository<HistorialEstadoFactura, Long>  {
+
+}
